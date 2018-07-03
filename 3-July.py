441f30a06e29d71d4eddaedeb44ad58e051b9277
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-03 10:43:29 +0530

    Basic syntax
    How to define a variable in python,
    Syntax of if-else,
    How to print string, int in a single line.

---
+++ 3-July.py
@@ -0,0 +1,23 @@
+# syntax to define a string variable
+str = "Hello World"
+
+# syntax to define a int variable
+num = 53
+
+# syntax to print string, int variable in a single line
+# use %s to display string variable
+# use %d to display int variable
+# use %() to define the number of variables
+print("String is: %s,  Number is: %d" %(str,num))
+
+# syntax to define if-else condition
+# don't forget to add :
+# : describes the scope of block
+if (num == 52):
+    print("yes")
+    print("hi")
+
+    print("\ngurjas")
+#print("\npython")
+else:
+    print("no")
