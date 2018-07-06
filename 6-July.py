ee5e55bbba0cf07e8fe9b5ab28bd0e14db66da23
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-06 10:29:17 +0530

    Update 6-July.py
    1. How to write function in python
    2. How to pass variables in python
    3. calling function.
    4. Creating class and passing value to member variables using constructor

--- 6-July.py
+++ 6-July.py
@@ -34,4 +34,37 @@ def starPatternDiamond():
 
 starPatternInverse()
 starPatternDiamond()
+
+# How to write function in python
+# How to pass variables in python
+##def sumTwoNumbers(var1 = 0, var2 = 5):
+##    var3 = var1 + var2
+##    print(var3)
+##
+# taking input from user and converting it to int
+##var1 = int(input("Enter value for variable 1: "))
+##var2 = int(input("Enter value for variable 2: "))
+##
+# calling function.
+###sumTwoNumbers(var1, var2)
+##sumTwoNumbers(var1)
+
+# defining class in python
+class Student:
+    # member variables in python
+    name = 'Gurjas'
+    college = 'DIT'
+
+    # constructor in python and passing values to member variables
+    def __init__(self, name):
+        self.name = name
+
+    # using self keyword while writing functions in python
+    def printDetails(self):
+        print("Details are as follows: ", self.name)
+
+# using objects of a class in python.
+# passing value to objects in python.
+obj = Student('Mohit')
+obj.printDetails()
         
