2867bb0c9ca09ec60bf9860b546cb0faa38711dc
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-16 10:27:49 +0530

    Tkinter, Database connection
    1. Introduction to SQLite3 database.
    2. Creating connection with database.
    3. Insert values into table.
    4. Read values from database.
    5. Using Treeview in tkinter.

--- Intro to Tkinter.py
+++ Intro to Tkinter.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 root = tk.Tk()
+root.title("Management")
 
 # def changeText():
 #     myName
@@ -19,35 +21,108 @@ root = tk.Tk()
 # buttonVariable.pack()
 # root.mainloop()
 
+appLabel = tk.Label(root, text="Student Management System", fg="#06a099", width=40)
+appLabel.config(font=("Sylfaen", 30))
+appLabel.grid(row=0, columnspan=2, padx=(30,30), pady=(30, 0))
 
-nameLabel = tk.Label(root, text="Enter your name", width=30, anchor='w')\
-    .grid(row=0, column=0, padx=(30,0))
-collegeLabel = tk.Label(root, text="Enter your college", width=30, anchor='w')\
-    .grid(row=1, column=0, padx=(30,0))
+class Student:
+    studentName = ""
+    collegeName = ""
+    phoneNumber = 0
+    address = ""
+
+    def __init__(self, studentName, collegeName, phoneNumber, address):
+        self.studentName = studentName
+        self.collegeName = collegeName
+        self.phoneNumber = phoneNumber
+        self.address = address
+
+nameLabel = tk.Label(root, text="Enter your name", width=40, anchor='w')\
+    .grid(row=1, column=0, padx=(30,0), pady=(30, 0))
+collegeLabel = tk.Label(root, text="Enter your college", width=40, anchor='w')\
+    .grid(row=2, column=0, padx=(30,0))
+phoneLabel = tk.Label(root, text="Enter your phone number", width=40, anchor='w')\
+    .grid(row=3, column=0, padx=(30,0))
+addressLabel = tk.Label(root, text="Enter your address", width=40, anchor='w')\
+    .grid(row=4, column=0, padx=(30,0))
 
 nameEntry = Entry(root)
 collegeEntry = Entry(root)
+phoneEntry = Entry(root)
+addressEntry = Entry(root)
 
-nameEntry.grid(row=0, column=1, padx=(0,50))
-collegeEntry.grid(row=1, column=1, padx=(0,50), pady = 20)
+nameEntry.grid(row=1, column=1, padx=(0,50), pady=(30, 20))
+collegeEntry.grid(row=2, column=1, padx=(0,50), pady = 20)
+phoneEntry.grid(row=3, column=1, padx=(0,50), pady = 20)
+addressEntry.grid(row=4, column=1, padx=(0,50), pady = 20)
+
 username = ""
 collegeName = ""
+phone = ""
+address = ""
+list = []
 
 def takeNameInput():
-    global nameEntry, collegeEntry
-    global username, collegeName
+    global nameEntry, collegeEntry, phoneEntry, addressEntry
+    global username, collegeName, phone, address
+    global list
     username = nameEntry.get()
     nameEntry.delete(0, END)
     collegeName = collegeEntry.get()
     collegeEntry.delete(0, END)
+    phone = int(phoneEntry.get())
+    phoneEntry.delete(0, END)
+    address = addressEntry.get()
+    addressEntry.delete(0, END)
+    obj = Student(username, collegeName, phone, address)
+    list.append(obj)
+
+def destroyRootWindow():
+    root.destroy()
 
 def printDetails():
-    print("Username is: %s and college name is: %s" % (username, collegeName))
+    for singleItem in list:
+        print("Student name is: %s\nCollege name is: %s\nPhone number is: %d\nAddress is: %s" %
+              (singleItem.studentName, singleItem.collegeName, singleItem.phoneNumber, singleItem.address))
+        print("****************************************")
 
 button = tk.Button(root, text="Take input", command=takeNameInput)
-button.grid(row=2, column=0)
+button.grid(row=5, column=0, pady=30)
 
-displayButton = tk.Button(root, text="Display result", command=printDetails)
-displayButton.grid(row=2, column=1)
+displayButton = tk.Button(root, text="Display result", command=destroyRootWindow)
+displayButton.grid(row=5, column=1)
 
 root.mainloop()
+
+secondWindow = tk.Tk()
+
+secondWindow.title("Display results")
+
+appLabel = tk.Label(secondWindow, text="Student Management System", fg="#06a099", width=40)
+appLabel.config(font=("Sylfaen", 30))
+# appLabel.grid(row=0, columnspan=2, padx=(30,30), pady=(30, 0))
+appLabel.pack()
+
+tree = ttk.Treeview(secondWindow)
+tree["columns"]=("one","two", "three", "four")
+tree.column("one", width=200)
+tree.column("two", width=200)
+tree.column("three", width=200)
+tree.column("four", width=200)
+tree.heading("one", text="Student Name")
+tree.heading("two", text="College Name")
+tree.heading("three", text="Phone Number")
+tree.heading("four", text="Address")
+
+# tree.insert('', 0, text="Student 1", values=("Gurjas Singh", "DIT, University", 9810821705, "Kapurthala, Punjab"))
+
+i = 0
+for singleItem in list:
+    tree.insert('', i, text="Student " + str(i+1),
+                values=(singleItem.studentName, singleItem.collegeName,
+                        singleItem.phoneNumber, singleItem.address))
+    i = i+1
+
+tree.pack()
+
+secondWindow.mainloop()
