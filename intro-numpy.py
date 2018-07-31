2c1d398fb030ece7942d45e46bebe6789844cfa5
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-31 10:07:36 +0530

    Intro to

---
+++ intro-numpy.py
@@ -0,0 +1,30 @@
+import numpy as np
+# from numpy import array
+
+height = [1.73, 2.86, 1.5, 1.94]
+weight = [65.4, 70.8, 62.9, 73.8]
+
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+# print(height)
+# print(weight)
+
+# print(np_height)
+# print(np_weight)
+
+# bmiList = weight/height
+# print(bmiList)
+
+# bmi = np_weight/np_height ** 2
+#
+# print(bmi)
+
+# weight = weight + weight
+# print(weight)
+
+num_array = np.array([1, 2, 3])
+
+num_array = num_array + num_array
+
+print(num_array)
